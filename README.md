aaceb0e0a7660a3b5e4c2a98f686ea0adc797f7f
pizza61 <34103636+pizza61@users.noreply.github.com>
2021-03-26 20:42:23 +0100

    Added episode filters

--- README.md
+++ README.md
@@ -15,4 +15,27 @@ Just clone this repository, do `pip install -r requirements.txt`. You can do thi
 ## Usage
 `python main.py "search query"`
 
+```
+usage: main.py [-h] [-q QUALITY] [-e EPISODES] [--dryrun] [--quiet] title [title ...]
+
+puya.moe batch download tool
+
+positional arguments:
+  title                 Exact title
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -q QUALITY, --quality QUALITY
+                        Quality (usually only 720p and 1080p is available)
+  -e EPISODES, --episodes EPISODES
+                        Specify episodes to download
+  --dryrun              Dry run (only for development)
+  --quiet, --noconfirm  Don't ask for confirmation
+```
+
 Default quality is 1080p. If you want to specify a different one, use -q, for example -q 720p.
+
+### Episode filters
+By default, puya-dl selects and downloads all episodes. When -e argument is passed, only selected episodes are downloaded. Just enter episode number to select an episode. You can also use ranges like `1-3`. Seperate filters with a single comma (,).
+
+For example, `python main.py title -e 1-2,5-6,10` will select episodes 1, 2, 5, 6 and 10.
--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import subprocess
 import re
 import requests
 from bs4 import BeautifulSoup
+import episodeFilter
 
-def req(query, q):
-    res = requests.get("https://nyaa.si/user/puyero?q="+query+"+"+q, headers={'User-Agent': 'puya-dl/1.0'})
+def req(query):
+    res = requests.get("https://nyaa.si/user/puyero?q="+query+"+"+args.quality, headers={'User-Agent': 'puya-dl/1.0'})
     parsed = BeautifulSoup(res.content, 'html.parser')
 
     items = []
@@ -44,34 +45,57 @@ def list_titles(items):
 
     return unq_list
 
-def download(title, items):
+def filter(items, title):
+    filtered = []
+        
+    if args.episodes:
+        episodes = episodeFilter.parseEpisodeFilter(args.episodes)
+        for x in items:
+            if x['title'] == title:
+                try:
+                    ep = int(x['episode'])
+                    if ep in episodes:
+                        filtered.append(x)
+                except:
+                    print("Couldn't parse episode number: ", x['episode'])
+    else:
+        for x in items:
+            if x['title'] == title:
+                filtered.append(x)
+    return filtered
+
+def download(items):
     first = 1
     for x in items[::-1]:
-        if x['title'] == title:
-            if first == 1:
-                print("\033[93mYour BitTorrent client should open with the first file. Please specify a directory so your client remembers it. Type 'cancel' if you want to abort or anything else to continue.\033[0m")
-                subprocess.run(['xdg-open', x['magnet']], stderr=subprocess.DEVNULL)
-                a = input("(cancel/continue)> ").lower()
-                if a == 'cancel':
-                    print("Cancel")
-                    break
-                else:
-                    first = 0
-                    print("\033[93mContinuing.\033[0m")
-                    continue
+        if first == 1 and args.noconfirm == False:
+            print("\033[93mYour BitTorrent client should open with the first file. Please specify a directory so your client remembers it. Type 'cancel' if you want to abort or anything else to continue.\033[0m")
+            subprocess.run(['xdg-open', x['magnet']], stderr=subprocess.DEVNULL)
+            a = input("(cancel/continue)> ").lower()
+            if a == 'cancel':
+                print("Cancel")
+                break
             else:
-                subprocess.run(['xdg-open', x['magnet']], stderr=subprocess.DEVNULL)
+                first = 0
+                print("\033[93mContinuing.\033[0m")
+                continue
+        else:
+            subprocess.run(['xdg-open', x['magnet']], stderr=subprocess.DEVNULL)
 
 # ARGPARSE
 parser = argparse.ArgumentParser(description="puya.moe batch download tool")
-parser.add_argument('title', metavar="Title", nargs='+', help="Exact title")
+parser.add_argument('title', nargs='+', help="Exact title")
 parser.add_argument('-q', '--quality', dest="quality", help="Quality (usually only 720p and 1080p is available)", default="1080p")
+parser.add_argument('-e', '--episodes', dest="episodes", help="Specify episodes to download", required=False)
+parser.add_argument('--dryrun', action='store_true', dest="dryrun", help="Dry run (only for development)")
+parser.add_argument('--quiet', '--noconfirm', action='store_true', dest="noconfirm", help="Don't ask for confirmation")
+
 args = parser.parse_args()
-
 query = ' '.join(args.title)
-q = args.quality
 
-items = req(query, q)
+if args.dryrun:
+    exit()
+
+items = req(query)
 titles = list_titles(items)
 
 if len(titles) > 1:
@@ -84,8 +108,10 @@ if len(titles) > 1:
         exit(0)
     else:
         print("Selected", titles[output])
-        download(titles[output], items)
+        filtered = filter(items, titles[output])
+        download(filtered)
 else:
-    download(titles[0], items)
+    filtered = filter(items, titles[0])
+    download(filtered)
 
 print("\033[93mFinished.\033[0m")
